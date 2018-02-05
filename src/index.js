fcdd4f8778fe4a1e4629ea364924228f433ec314
Siegrift <e.tesarr@gmail.com>
2018-02-05 21:41:26 +0100

    Set up react redux  boilerplate

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import App from './App'
+import { AppContainer } from 'react-hot-loader'
+import getConfiguredStore from './configureStore'
+import 'assets/scss/style.scss'
+// load bootstrap for reactstrap
+import 'bootstrap/dist/css/bootstrap.css'
+// extend bluebird promise in dev mode
+Promise.config({
+  longStackTraces: process.env.NODE_ENV === 'development',
+  warnings: process.env.NODE_ENV === 'development',
+})
+
+const store = getConfiguredStore()
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+
+}
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {render(App)})
+}
+
+render(App)
