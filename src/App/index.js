61759102b00ef9f9529b6f86fe9009037eb00912
Siegrift <e.tesarr@gmail.com>
2018-02-05 22:00:26 +0100

    Render diagram editor

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {Button} from 'reactstrap'
-import {connect} from 'react-redux'
-import {buttonTextSelector} from './state'
-import {toggleButtonText} from './actions'
+import { Button } from 'reactstrap'
+import { connect } from 'react-redux'
+import { buttonTextSelector } from './state'
+import { toggleButtonText } from './actions'
+import DiagramEditor from '../../react-diagrams/src'
 
-const App = ({buttonText, toggleButtonText}) => (
+const App = ({ buttonText, toggleButtonText }) => (
   <div>
     <Button onClick={toggleButtonText}>{buttonText}</Button>
+    <DiagramEditor />
   </div>
 )
 
