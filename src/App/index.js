38f2d3546c3611b3ef100fe3bc7b30f31d79f428
Siegrift <e.tesarr@gmail.com>
2018-06-19 12:03:01 +0200

    Finish school project

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import DiagramEditor from 'react-diagrams/src'
 import { schema } from './schema'
-import { Button } from 'reactstrap'
+import { Button, Input } from 'reactstrap'
 
 class App extends React.Component {
   static editorApi = undefined
+  state = { x: '' }
 
   handleGrafExport = (e) => {
     console.log(this.editorApi.exportGraph())
   }
 
   testZero = () => {
+    if (!this.state.x || this.state.x >= 1000) {
+      // eslint-disable-next-line
+      alert('Neplatny vstup')
+      return
+    }
     const graph = this.editorApi.exportGraph()
     const begins = Object.keys(graph.widgets).filter((key) => graph.widgets[key].key === 'begin')
     let message = 'Zle! :('
+    let state = { input: this.state.x }
     if (begins.length === 1) {
-      let state = { command: graph.widgets[begins[0]], input: 980 }
+      state = { command: graph.widgets[begins[0]], input: this.state.x }
       let maxIter = schema.maxSteps
       while (maxIter !== 0 && state.command && state.command.key !== 'end') {
         maxIter -= 1
@@ -24,7 +31,7 @@ class App extends React.Component {
       if (state.command && state.command.key === 'end' && state.input === 0) message = 'Super! :)'
     }
     // eslint-disable-next-line
-    alert(message)
+    alert(message + `(cislo x je ${state.input})`)
   }
 
   executeCommand = (state) => {
@@ -46,30 +53,60 @@ class App extends React.Component {
       return ports[portIndex].editorKey
     }
     switch (command.key) {
-      case 'begin': return { command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))), input }
-      case 'end': return state
+      case 'begin':
+        return { command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))), input }
+      case 'end':
+        return state
       case 'is_zero': {
         const portKey = input === 0 ? 'zero' : 'nonzero'
-        return { command: getNodeByPort(findPortTarget(findPort(command.outPorts, portKey))), input }
+        return {
+          command: getNodeByPort(findPortTarget(findPort(command.outPorts, portKey))),
+          input,
+        }
       }
-      case 'dec': return { command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))), input: input - 1 }
-      case 'inc': return { command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))), input: input + 1 }
-      default: return undefined
+      case 'dec':
+        return {
+          command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))),
+          input: input - 1,
+        }
+      case 'inc':
+        return {
+          command: getNodeByPort(findPortTarget(findPort(command.outPorts, 'out'))),
+          input: input + 1,
+        }
+      default:
+        return undefined
     }
+  }
+
+  setX = (e) => this.setState({ x: parseInt(e.target.value, 10) })
+
+  handleRef = (ref) => {
+    this.editorApi = ref
   }
 
   render() {
     return (
       <div>
-        <DiagramEditor schema={schema} ref={(ref) => {this.editorApi = ref}} />
-        <div className="Toolbar" >
+        <DiagramEditor schema={schema} ref={this.handleRef} />
+        <div className="Toolbar">
           API:
-          <Button onClick={this.handleGrafExport} >Export graph</Button>
+          <Button onClick={this.handleGrafExport}>Export graph</Button>
         </div>
         <div className="Problems">
-          <div >
-            <p>Na vstupe je kladne cislo x mensie ako 1000, vystup vasho programu ma byt cislo 0.</p>
-            <Button onClick={this.testZero} >Otestuj</Button>
+          <div>
+            <p>
+              Na vstupe je kladne cislo x mensie ako 1000, vystup vasho programu ma byt cislo 0.
+            </p>
+            <div className="sameLine">
+              <Input
+                type="number"
+                value={this.state.x}
+                onChange={this.setX}
+                placeholder="Zvolte x"
+              />
+              <Button onClick={this.testZero}>Otestuj</Button>
+            </div>
           </div>
         </div>
       </div>
